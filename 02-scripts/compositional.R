e7bc5fa899a68c9976f610337307ae9671fc0efb
Erika Kvalem <erika.kvalem@gmail.com>
2025-07-16 13:26:01 +0200

    comment code

--- 02-scripts/compositional.R
+++ 02-scripts/compositional.R
@@ -12,20 +12,26 @@ library(dplyr)
 ps1 <- readRDS("/data/projects/2024/Effenberger-Diabetes/out/nf_core_ampliseq_003/phyloseq/dada2_phyloseq.rds")
 print(ps1)
 ps1.com <- ps1
+
+#Rename taya as ASVs and extract taxonomy table
 taxa_names(ps1.com) <- paste0("ASV_", rownames(tax_table(ps1.com)))
 taxic <- as.data.frame(ps1.com@tax_table)
-
 taxic$OTU <- rownames(taxic) 
 colnames(taxic)
+
+#Update and clean tayonomy table
 taxmat <- as.matrix(taxic) 
 new.tax <- tax_table(taxmat)
 tax_table(ps1.com) <- new.tax 
 tax_table(ps1.com)[tax_table(ps1.com)[, "Family"] == "", "Family"] <- "Unclassified family"
+
+# set legend formatting
 guide_italics <- guides(fill = guide_legend(label.theme = element_text(
   size = 15,
   face = "italic", colour = "Black", angle = 0
 )))
 
+#Remove phylogenetic tree if it exists
 ps1.com@phy_tree <- NULL
 
 # Transform to relative abundance 
@@ -35,13 +41,15 @@ ps1.com <- microbiome::transform(ps1.com, "compositional")
 # Filter out taxa with mean relative abundance < 0.1% 
 ps1.com <- filter_taxa(ps1.com, function(x) mean(x) > 0.001, prune = TRUE)
 
-
+#Agregate taxa at Family level and select top 10 families
 ps_fam <- aggregate_taxa(ps1.com, level = "Family")
 top_families <- names(sort(taxa_sums(ps_fam), decreasing = TRUE))[1:10]
 ps1.com.fam <- prune_taxa(top_families, ps_fam)
 
+#Relative abundance transformation
 ps1.com.fam.rel <- microbiome::transform(ps1.com.fam, "compositional")
 
+#Plot
 plot.composition.relAbun <- plot_composition(ps1.com.fam.rel,
                                              sample.sort = "scientific_name",
                                              x.label = "env_material") 
@@ -54,7 +62,7 @@ print(plot.composition.relAbun)
 
 
 
-
+# Reorder samples based on clnical information
 ps_fam <- aggregate_taxa(ps1.com, level = "Family")
 top_families <- names(sort(taxa_sums(ps_fam), decreasing = TRUE))[1:10]
 ps1.com.fam <- prune_taxa(top_families, ps_fam)
